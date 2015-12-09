c153c57b9dd85817dacc282b6e0d4fcb4fa032be
Chad Hietala <chadhietala@gmail.com>
2015-12-08 17:34:28 -0800

    Such AST walking

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,7 +4,9 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
-    // Add options here
+    vendorFiles: {
+      'ember.js': 'vendor/ember.js'
+    }
   });
 
   // Use `app.import` to add additional libraries to the generated
